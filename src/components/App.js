903745f94779378b7b28ae35c756bd5a9ea7a31f
Rodrigo Martin Diaz <rodrigo.m.diaz@hotmail.com>
2022-02-28 21:42:51 -0300

    correccion de estilo y bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import "../style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
