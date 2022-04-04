167162eb9ff1647256c022ee7f16cc1290fbb2f7
Rodrigo Martin Diaz <rodrigo.m.diaz@hotmail.com>
2022-04-03 23:03:59 -0300

    se mejoro navbar y se cambio fuente

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import CartWidget from "./CartWidget";
 import { AiOutlineShoppingCart } from "react-icons/ai";
@@ -9,11 +9,52 @@ function NavBar() {
   const { cantidad, cargando } = useContext(contexto);
 
   return (
-    <Container className="navbar" fluid>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <NavLink to="/">
+          <CartWidget />
+        </NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Quienes Somos</Nav.Link>
+
+            <NavDropdown
+              variant="dark"
+              title="Categorias"
+              id="collasible-nav-dropdown"
+            >
+              <NavLink to="/categoria/Papelillos">Papelillos</NavLink>
+              <NavLink to="/categoria/Filtros">Filtros</NavLink>
+              <NavLink to="/categoria/Pipa">Pipas</NavLink>
+              <NavDropdown.Divider />
+              <NavLink to="/">Todos los productos</NavLink>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            {cargando ? (
+              " "
+            ) : (
+              <NavLink id="cart-icon" to="/carrito">
+                <AiOutlineShoppingCart />
+                Carrito <span className="red">{cantidad}</span>
+              </NavLink>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
+
+{
+  /* <Container className="navbar" fluid>
       <div className="navbar-presentacion">
         <NavLink to="/">
           <CartWidget />
-          {/* <h1>GrowShopOnline</h1> */}
+          { <h1>GrowShopOnline</h1> }
         </NavLink>
       </div>
       <div className="navbar-menu">
@@ -35,8 +76,5 @@ function NavBar() {
           </NavLink>
         )}
       </div>
-    </Container>
-  );
+    </Container> */
 }
-
-export default NavBar;
